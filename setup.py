ed5fa43232ca8e6930232b2a020a958284d027dd
akash3461 <akashchavan4278@gmail.com>
2026-07-11 19:06:30 +0530

    new files created

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MediGuide-AI",
+    version="0.0.1",
+    author="Akash Chavan",
+    packages=find_packages(),
+    install_requires=[],
+)
